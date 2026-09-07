5df5b13d35644f36979d23565084d27b930f62eb
Rzfan03 <fajrulsyaban47@gmail.com>
2026-09-07 21:26:49 +0700

    add: add navbar

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const datas = 
+  {
+    Name: "Rzfan03",
+    Github: "Rzfan03",
+    Desc: "Testing!"
+  }
+
+
+export default datas
---
+++ src/types/NavLinks.ts
@@ -0,0 +1,4 @@
+export type NavLinks = {
+  text: string
+  Href: string
+}
